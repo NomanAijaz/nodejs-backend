3ff400265db4ad71259b7a66f5170d9005483d51
NomanAijaz <noman.aijaz332@gmail.com>
2025-02-13 18:56:07 +0500

    utils code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import dotenv from 'dotenv'
-import express from 'express'
+import {app} from './app.js'
+
 import connectDatabase from './database/index.js'
 dotenv.config({
     path:'.env'
 })
 
-const app = express()
-connectDatabase()
 
-app.get('/', (req, res)=>{
-        res.send("Hello world")
-})
-
-const PORT  = process.env.PORT || 8080
+connectDatabase().then(()=>{
+    const PORT  = process.env.PORT || 8080
 
 app.listen(PORT, ()=>{
     console.log(`App is running on ${PORT}`);
     
 })
+}).catch(err=>{
+    console.log("Database connection failed: ",err);
+    
+})
+
